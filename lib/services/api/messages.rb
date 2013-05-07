f8ee6f69cb32a4e0aaa9eb6dc4f71bee03e3eb9c
David Stevenson and Phan Le <pair+stevend+phan@pivotallabs.com>
2013-05-07 16:51:51 -0700

    Revert "Make unique_id required in service related messages"
    This reverts commit d446a074364d1ad6baa14a7c096f7caf17027783.

--- lib/services/api/messages.rb
+++ lib/services/api/messages.rb
@@ -22,7 +22,6 @@ module VCAP
         required :url,          URI::regexp(%w(http https))
         required :supported_versions, [String]
         required :version_aliases, Hash
-        required :unique_id,    String
 
         optional :description,  String
         optional :info_url,     URI::regexp(%w(http https))
@@ -30,11 +29,11 @@ module VCAP
         optional :plan_details do
           [
             {
-              "unique_id" => String,
               "name" => String,
               "free" => bool,
               optional("description") => String,
               optional("extra") => String,
+              optional("unique_id") => String,
             }
           ]
         end
@@ -49,6 +48,7 @@ module VCAP
         optional :provider,     String
         optional :default_plan, String
         optional :extra,        String
+        optional :unique_id,    String
       end
 
       class ProxiedServiceOfferingRequest < JsonMessage
@@ -97,7 +97,6 @@ module VCAP
         required :plan,  String
         required :email, String
         required :version, String
-        required :unique_id, String
 
         optional :provider, String
 
@@ -105,6 +104,7 @@ module VCAP
 
         optional :organization_guid, String
         optional :space_guid, String
+        optional :unique_id, String
       end
 
       # Provision and bind response use the same format
